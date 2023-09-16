69b78d9e103d0a4a8ac0f45f3b79731c43e1e76f
yuseon <dbtjs3@naver.com>
2023-09-17 03:25:43 +0900

    Feat:메인

--- js/main.js
+++ js/main.js
@@ -1,24 +1,8 @@
 const allEl = document.querySelectorAll("a");
-const logo = document.querySelectorAll(".logo");
-
 
 allEl.forEach((a) => {
     a.addEventListener("click", (e) => {
         e.preventDefault();
     });
 });
-
-const toLogin = document.getElementsByClassName("to-login")[0];
-
-toLogin.addEventListener("click", () => {
-    window.location.href = "../html/login.html";
-})
-
-logo.forEach((item) => {
-    item.addEventListener("click", () => {
-        console.log("logologo")
-        window.location.href = "../index.html";
-    });
-});
-
 
--- js/sub_login.js
+++
@@ -1,5 +0,0 @@
-const toRegister = document.getElementsByClassName("btn_register")[0];
-
-toRegister.addEventListener("click", () => {
-    window.location.href = "../html/register.html";
-})
